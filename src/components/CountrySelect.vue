51276d233dfe86e751cf28d8a45aa58ba79d3f5f
Ana-Maria <ana.maria.bulat@accenture.com>
2021-10-05 17:45:17 +0200

    init

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,29 @@
+<template>
+    <select v-model="selected" @change="onChange()" class="form-select mt-10 block w-full border p-3 rounded">
+      <option value="0">Select Country</option>
+        <option v-for="country in countries"
+          :value="country.ID"
+          :key="country.ID">
+          {{ country.Country }}
+        </option>
+    </select>
+</template>
+<script>
+export default {
+    
+    name: 'CountrySelect',
+    props: ['countries'],
+    data() {
+        return {
+            selected: 0
+        }
+    }, 
+    methods: {
+        onChange() {
+            const country2 = this.countries?.find( i =>  i.ID=== this.selected)
+            console.log(country2)
+            this.$emit('get-country',country2)
+        }
+    }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="text-center bg-blue-800 text-white p-4 mb-10">
+    <div class="text-3xl md:text-4xl font-bold mb-3">
+      <i class="fa fa-viruses"/> Covid-19 Tracker</div>
+      <p>API by 
+        <a class="text-blue-300" 
+      href="https://covid19api.com"
+      targer="_blank">covid19api.com</a></p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
